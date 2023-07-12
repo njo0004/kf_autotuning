573a86c96e4c1f8230afc6d21d228c6bff94a60a
njo0004 <njo0004@auburn.edu>
2023-07-12 12:55:18 -0500

    WIP

--- src/main.m
+++ src/main.m
@@ -17,7 +17,7 @@ H = [1 0];
 Gamma = [0;1];
 
 dt = 0.1;
-dt_sim = 0.001;
+dt_sim = 0.1;
 
 V = 1;
 W = 0.1;
@@ -42,8 +42,8 @@ for i = 1:N
         Xdot = A*X(:,j,i) + B*u(i,j) + Gamma*V*randn;
         X(:,j+1,i) = X(:,j,i) + Xdot*dt_sim;
 
-        if(~mod(dt,dt_sim*j))
-
+        if(~mod(dt_sim*j,dt))
+            
             Y(i,meas_idx) = H*X(:,j,i) + (W/dt)*randn(1);
             
             meas_idx = meas_idx + 1;
@@ -86,16 +86,55 @@ end
 F = [1 dt;0 1];
 G = [0.5*dt^2;dt];
 Q = VanLoanDiscretization(V,B,A,dt);
+R = W/dt;
 
 X_hat(:,1,:) = zeros(2,1,i);
 
 for i = 1:N
+    
+    P(:,:,1,i) = 5*eye(2); % OOF
 
+    for j = 1:20/dt
 
+        X_hat(:,j+1,i) = F*X_hat(:,j,i) + G*signal_amp*sin(j*dt*signal_freq);
+        
+        delX(:,j,i) = X(:,j,i) - X_hat(:,j,i);
 
+        P(:,:,j+1,i) = F*P(:,:,j,i)*F' + Q;
+        
+        NEES(i,j) = delX(:,j,i)'*inv(P(:,:,j,i))*delX(:,j,i);
 
+        S(:,:,j,i) = (H*P(:,:,j,i)*H' + R);
 
+        K = P(:,:,j+1,i)*H'/(S(:,:,j,i));
 
+        delY(i,j) = Y(i,j) - H*X_hat(:,j+1,i);
 
+        NIS(i,j) = delY(i,j)'/(S(:,:,j,i))*delY(i,j);
+
+        X_hat(:,j+1,i) = X_hat(:,j+1,i) + K*delY(i,j);
+
+        P(:,:,j+1,i) = (eye(2) - K*H)*P(:,:,j+1,i);
+
+    end
+    
+    figure(3)
+    plot(NEES(i,:))
+    hold on
+    title("NEES")
+
+    figure(4)
+    plot(NIS(i,:))
+    hold on
+    title("NIS")
 
 end
+
+NEES_ave = (1/N).*sum(NEES,1);
+NIS_ave  = (1/N).*sum(NIS,1);
+
+figure(3)
+plot(NEES_ave)
+
+figure(4)
+plot(NIS_ave)
